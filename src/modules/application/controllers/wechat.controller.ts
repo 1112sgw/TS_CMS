e7151a67858a2bf5d231eecf377375ec274bb68f
Gavin <gavin.sun@innocellence.com>
2018-08-10 09:35:00 +0800

    update

--- src/modules/application/controllers/wechat.controller.ts
+++ src/modules/application/controllers/wechat.controller.ts
@@ -8,28 +8,6 @@ const api = new WechatAPI(process.env.WECHAT_APPID, process.env.WECHAT_APPSERCRE
 export class WechatController {
   constructor() {}
 
-  // @Get()
-  // tokenCheck(@Req() req, @Res() res) {
-  //   function checkSignature(reqS){
-  //       const token = process.env.TOKEN,
-  //       timestamp = reqS.query.timestamp,
-  //       nonce     = reqS.query.nonce,
-  //       signature = reqS.query.signature;
-  //       const sha1 = crypto.createHash('sha1');
-  //       const arr = [token, timestamp, nonce].sort();
-  //       sha1.update(arr.join(''));
-  //       return sha1.digest('hex') === signature;
-  //   }
-
-  //   if (!checkSignature(req)){
-  //     res.writeHead(401);
-  //     res.end('signature not matched.');
-  //     return;
-  //   }
-  //   res.writeHead(200);
-  //   res.end(req.query.echostr);
-  // }
-
   @Get('/sync')
   syncFollowers(@Req() req, @Res() res) {
     api.getFollowers((err, data, resp) => {
