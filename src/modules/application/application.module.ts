64925cb4cf690974f1508e4a412fad24c12f4204
Gavin <gavin.sun@innocellence.com>
2018-08-15 10:51:03 +0800

    update

--- src/modules/application/application.module.ts
+++ src/modules/application/application.module.ts
@@ -4,12 +4,13 @@ import { RegionService } from './services/region.service';
 import { RegionController } from './controllers/region.controller';
 import { WechatController } from './controllers/wechat.controller';
 import { Region } from './entities/region.entity';
+import { WechatService } from './services/wechat.service';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Region], process.env.DATABASE_SCHEMA_APP),
   ],
   controllers: [RegionController, WechatController],
-  providers: [RegionService],
+  providers: [RegionService, WechatService],
 })
 export class AppModule {}
---
+++ src/modules/application/services/wechat.service.ts
@@ -0,0 +1,38 @@
+import { Injectable, Inject } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository , DeleteResult, UpdateResult, LessThan } from 'typeorm';
+import { Token } from '../entities/token.entity';
+import * as WechatAPI from 'co-wechat-api';
+
+@Injectable()
+export class WechatService {
+  constructor(
+    @InjectRepository(Token)
+    private readonly tokenRepository: Repository<Token>,
+  ) {}
+
+  api(): any {
+    return new WechatAPI(process.env.WECHAT_APPID, process.env.WECHAT_APPSERCRET, async (callback) => {
+      const dateNow = new Date();
+      const tokens = await this.tokenRepository.find({
+        where: {
+          ExpireTime: LessThan(dateNow),
+          type: 'wechat',
+        },
+      });
+      if (tokens && tokens.length > 0) {
+        return JSON.parse(tokens[0].Token);
+      }
+    }, async (token, callback) => {
+        if (token && token.errcode && token.errcode === 0){
+          await this.tokenRepository.insert({
+            Token: token.ACCESS_TOKEN,
+            AccessTime: new Date(),
+            ExpireTime: token.expires_in,
+            Type: 'wechat',
+          });
+        }
+    });
+  }
+
+}
