98f4fcf889034817af7a5f8d63eadf2e484e2768
Gavin <gavin.sun@innocellence.com>
2018-08-15 10:43:41 +0800

    update

--- src/middleware/wechat.ts
+++ src/middleware/wechat.ts
@@ -10,18 +10,45 @@ const initWechat = (app) => {
     app.use('/wechat', wechat(config, (req, res, next) => {
         // message is located in req.weixin
         const message = req.weixin;
-        if (message.MsgType === 'text') {
-            // device text
-            res.reply('This message will be pushed onto the device.');
-        }
-        else if (message.MsgType === 'device_event') {
-            if (message.Event === 'subscribe_status' ||
-                message.Event === 'unsubscribe_status') {
-                // subscribe or unsubscribe the WIFI device status,the reply should be 1 or 0
-                res.reply(1);
-            } else {
+        if (message.MsgType === 'event') {
+            if (message.Event === 'subscribe') {
+                if (message.EventKey && message.Ticket){
+                    res.reply('Thanks for your subscribe by scan qrcode');
+                }
+                else{
+                    res.reply('Thanks for your subscribe');
+                }
+            }
+            else if (message.Event === 'unsubscribe'){
                 res.reply('This message will be pushed onto the device.');
             }
+            else if (message.Event === 'LOCATION'){
+                res.reply('This message will be pushed onto the device.');
+            }
+            else if (message.Event === 'CLICK'){
+                res.reply('Click the menu.');
+            }
+        }
+        else if (message.MsgType === 'text') {
+            res.reply('This is a text');
+        }
+        else if (message.MsgType === 'image') {
+            res.reply('This is a text');
+        }
+        else if (message.MsgType === 'voice') {
+            res.reply('This is a text');
+        }
+        else if (message.MsgType === 'video') {
+            res.reply('This is a text');
+        }
+        else if (message.MsgType === 'location') {
+            res.reply('This is a text');
+        }
+        else if (message.MsgType === 'link') {
+            res.reply('This is a text');
+        }
+        else if (message.MsgType === 'shortvideo') {
+            res.reply('This is a text');
         }
     }));
 
--- src/modules/application/controllers/wechat.controller.ts
+++ src/modules/application/controllers/wechat.controller.ts
@@ -1,16 +1,13 @@
 import { Controller, Get, Post, Patch, Delete, Param, Req, Res, Body, HttpStatus, UseGuards } from '@nestjs/common';
-import * as WechatAPI from 'wechat-api';
-import * as crypto from 'crypto';
-import * as wechat from 'wechat';
-const api = new WechatAPI(process.env.WECHAT_APPID, process.env.WECHAT_APPSERCRET);
+import { WechatService } from '../services/wechat.service';
 
 @Controller('api/wechat')
 export class WechatController {
-  constructor() {}
+  constructor(private readonly WechatService: WechatService) {}
 
   @Get('/sync')
   syncFollowers(@Req() req, @Res() res) {
-    api.getFollowers((err, data, resp) => {
+    this.WechatService.api().getFollowers((err, data, resp) => {
       return res.json({
         code: 200,
         timestamp: new Date().toISOString(),
